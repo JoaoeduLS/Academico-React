184a94e5372b362d644e6cb792b5e481c030351f
joao <joaoeduardo.lsilva@gmail.com>
2023-05-16 21:46:28 -0300

    usando useform no formulario

--- pages/cursos/form.js
+++ pages/cursos/form.js
@@ -1,25 +1,28 @@
 import Pagina from "@/components/Pagina";
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const form = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <Pagina titulo="Formulario">
       <Form>
         <Form.Group className="mb-3">
           <Form.Label>Nome</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Control type="text" {...register("nome")} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Numero</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Control type="text" {...register("Numero")} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Control type="text" {...register("Email")} />
         </Form.Group>
         <Form.Label>Curso</Form.Label>
-        <Form.Select id="disabledSelect">
+        <Form.Select id="disabledSelect" {...register("Curso")}>
           <option></option>
           <option>Culinaria</option>
           <option>Ciencia da Computacao</option>
