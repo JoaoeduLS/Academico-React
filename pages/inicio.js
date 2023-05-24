880d1c4b03febc0368dd154e843747a3f31d74a0
joao <joaoeduardo.lsilva@gmail.com>
2023-05-23 22:03:38 -0300

    iniciando o leiloart

--- pages/inicio.js
+++ pages/inicio.js
@@ -10,7 +10,7 @@ const index = () => {
         <Card style={{ width: "18rem" }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
-            <Card.Title>vlavallva</Card.Title>
+            <Card.Title>A Noite Estrelada</Card.Title>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
